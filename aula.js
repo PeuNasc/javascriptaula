dd3a2868969fd0c8ea9d7d307fe3cfdd2a6f7007
PeuNasc <pedronasc.oliveira@hotmail.com>
2025-04-10 08:11:01 -0300

    Add files via upload

--- aula.js
+++ aula.js
@@ -4,3 +4,17 @@ alert("Ola!")
 let nome = prompt("Qual seu nome? ")
 alert("Bom dia " + nome + "!")
 confirm("Deseja continuar? ")
+console.log(nome)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    const inputDarkMode = document.getElementById('input-dark-mode')
+				
+    inputDarkMode.addEventListener('change', () => {
+        if(inputDarkMode.checked){
+            html.setAttribute("dark", "true")
+        }else{
+            html.removeAttribute("dark")
+        }
+    })
+})
